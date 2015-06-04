45b3e2097cb00fef5c534564046b68b4988a2181
Julien Syx <julien.syx@gmail.com>
2015-06-04 17:56:46 +0200

    Plugin api urls are more friendly

--- lisa_api/lisa/urls.py
+++ lisa_api/lisa/urls.py
@@ -35,4 +35,4 @@ urlpatterns = [
 ]
 
 for plugin in PluginManager().plugins:
-        urlpatterns.append(url(r'^api/v1/plugins/%s' % plugin, include('lisa_plugins_%s.urls' % plugin, namespace='plugins')))
+        urlpatterns.append(url(r'^api/v1/plugin-%s' % plugin, include('lisa_plugins_%s.urls' % plugin, namespace=plugin)))
