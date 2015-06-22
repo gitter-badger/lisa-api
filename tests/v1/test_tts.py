373649f342ec4c4ba524fd2bc848191fe06c58db
Julien Syx <julien.syx@gmail.com>
2015-06-22 09:17:53 +0200

    Adding another test for pico using another lang than "en"

--- tests/v1/test_tts.py
+++ tests/v1/test_tts.py
@@ -11,13 +11,22 @@ class CoreTests(APITestCase):
         data = {'message': u'bonjour', 'lang': 'en', 'driver': 'google'}
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        
+
     def test_v1_tts_post_pico(self):
         """
         Ensure we can send a message to tts
         """
         url = '/api/v1/core/tts/'
         data = {'message': u'bonjour', 'lang': 'en', 'driver': 'pico'}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_v1_tts_post_pico_other_lang(self):
+        """
+        Ensure we can send a message to tts
+        """
+        url = '/api/v1/core/tts/'
+        data = {'message': u'bonjour', 'lang': 'fr', 'driver': 'pico'}
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
