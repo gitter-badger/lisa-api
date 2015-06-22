5950043730df847016a43c637b387aad8acce874
Julien Syx <julien.syx@gmail.com>
2015-06-22 10:06:06 +0200

    Proper handling of failure in the except

--- lisa_api/api/tts/pico.py
+++ lisa_api/api/tts/pico.py
@@ -27,16 +27,17 @@ class Pico(base.TTSBase):
             language = 'en-US'
         else:
             language = '-'.join([str(lang), str(lang).upper()])
-        #try:
-        commandwav = ['pico2wave', '-w', tempwav.name, '-l', language, '--', message]
-        logger.debug("Command used to generate the wav sound : %s in the file %s" % (commandwav, tempwav.name))
-        logger.debug(subprocess.check_output(commandwav, stderr=subprocess.STDOUT))
-        commandmp3 = ['avconv', '-y', '-f', 'wav', '-i', tempwav.name, '-b:a', '256k', '-f', 'mp3', tempmp3.name]
-        logger.debug("Command used to generate the mp3 sound : %s in the file %s" % (commandmp3, tempmp3.name))
-        logger.debug(subprocess.check_output(commandmp3, stderr=subprocess.STDOUT))
-        #except subprocess.CalledProcessError:
-        #    logger.error('Problem generating sound')
-        #    return False
+        try:
+            commandwav = ['pico2wave', '-w', tempwav.name, '-l', language, '--', message]
+            logger.debug("Command used to generate the wav sound : %s in the file %s" % (commandwav, tempwav.name))
+            logger.debug(subprocess.check_output(commandwav, stderr=subprocess.STDOUT))
+            commandmp3 = ['avconv', '-y', '-f', 'wav', '-i', tempwav.name, '-b:a', '256k', '-f', 'mp3', tempmp3.name]
+            logger.debug("Command used to generate the mp3 sound : %s in the file %s" % (commandmp3, tempmp3.name))
+            logger.debug(subprocess.check_output(commandmp3, stderr=subprocess.STDOUT))
+        except subprocess.CalledProcessError as error:
+            logger.error('Problem generating sound')
+            logger.debug('Output : %s' % error.output)
+            return False
         combined_sound.append(tempmp3.read())
         tempwav.close()
         tempmp3.close()
